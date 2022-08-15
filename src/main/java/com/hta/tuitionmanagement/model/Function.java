d88631cec51146dbf212004cd123dd7c2309b53f
Minh <tranminh200199@gmail.com>
2022-08-15 22:17:19 +0700

    add role,func

---
+++ src/main/java/com/hta/tuitionmanagement/model/Function.java
@@ -0,0 +1,44 @@
+package com.hta.tuitionmanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="`function`")
+public class Function extends Auditable<String> implements Serializable {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "menu_name", columnDefinition="nvarchar(500)")
+    private String menuName;
+
+    @Column(name = "menu_code")
+    private String menuCode;
+
+    @Column(name = "action_code")
+    private String actionCode;
+
+    @Column(name = "description", columnDefinition="nvarchar(500)")
+    private String description;
+
+    @Column(name = "parent_code")
+    private String parentCode;
+
+//    @ManyToMany(mappedBy = "functions", fetch = FetchType.LAZY)
+//    private Set<Role> roles = new HashSet<>();
+
+
+}
---
+++ src/main/java/com/hta/tuitionmanagement/model/Role.java
@@ -0,0 +1,45 @@
+package com.hta.tuitionmanagement.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "`role`")
+public class Role extends Auditable<String> implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", columnDefinition="nvarchar(100)")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "description", columnDefinition="nvarchar(500)")
+    private String description;
+
+    @OneToOne(mappedBy = "role")
+    private UserEntity user;
+
+//    @ManyToMany(cascade = CascadeType.ALL)
+//    @JoinTable(name = "role_function", joinColumns = {@JoinColumn(name="role_id", referencedColumnName="id")},inverseJoinColumns ={@JoinColumn(name="function_id", referencedColumnName="id")})
+//    @JsonIgnore
+//    private Set<Function> functions = new HashSet<>();
+
+
+}
